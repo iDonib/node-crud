283473f426d2981b57bffb8cfa07d7e21b301cce
donib-irakihda <donib.irakihda@gmail.com>
2023-04-08 18:53:42 +0545

    completed simple crud project

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 
 //routes
 app.get('/', (req, res) => {
-    res.send("Hello Node API")
+    res.send("Hello pee0ps")
 })
 
-app.listen(3000, ()=> {
-    console.log('Node API is running on port 3000')
+app.get('/blog', (req, res) => {
+    res.send("Hello blog, I am Donib. Nice to meet you.")
+})
+
+
+mongoose.
+connect('mongodb+srv://admin:code@donibapi.qqipmei.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+    console.log("Connected to mongoDB")
+    app.listen(3000, ()=> {
+        console.log('Node API is currently running on port 3000')
+    });
+    
+}).catch(() => {
+    console.log(error)
 })
